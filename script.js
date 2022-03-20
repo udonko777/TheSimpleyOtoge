06724be8eb0a891c42f67031440d35690ae6d08a
asarinoudonko <kaipon110@gmail.com>
2022-03-20 15:44:33 +0900

    判定の追加と譜面描画アルゴリズムの修正
    開始直後、異なるハイスピードでプレイした際に判定位置が変わらないことを確認するためのデモが流れます。
    現状判定はコンソールでしか確認できません

--- script.js
+++ script.js
@@ -21,21 +21,23 @@ var falltime;
 
 class Note {
 
-    constructor(canvas, ctx, no, falltime) {
+    constructor(canvas, ctx, no, falltime, hispeed) {
 
         var clock = new Date();
         this.canvas = canvas;
         this.ctx = ctx;
         this.no = no;
+        this.hispeed = hispeed;
         this.falltime = falltime;
-        this.y = falltime + ((clock.getTime() - starttime) / 12); //canvasのhighist参照
+        //(落ちるまでの時間 + 現在の時間 - 開始時間) / ハイスピ + 判定位置
+        this.y = ((falltime + clock.getTime() - starttime) / hispeed) + 500;
     }
 
     writenote() {
         //ノーツの色の設定
         this.ctx.fillStyle = '#DD7070';
 
-        this.y = this.falltime + ((clock.getTime() - starttime) / 12);
+        this.y = ((this.falltime + clock.getTime() - starttime) / this.hispeed) + 500;
         //ノーツの描画
         this.ctx.fillRect(this.no * 52, this.y, 52, 10);;
         //console.log(this.y);
@@ -59,21 +61,19 @@ function frame() {
     ctx.clearRect(0, 0, 3000, 3000);
     ctx.fillStyle = 'rgb( 0, 0, 0)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgb( 0, 255, 0)';
+    ctx.fillRect(0, 500, canvas.width, 5);
     //譜面生成(要改善)
     /*
     var R = Math.floor(Math.random() * 90);
     if (R < 4 && notes.length < 10000) { notes.push(new Note(canvas, ctx, R, 12)); }
     */
 
-    notes.push(new Note(canvas, ctx, 0, 12));
-    notes.push(new Note(canvas, ctx, 1, 112));
-    notes.push(new Note(canvas, ctx, 2, 212));
-    notes.push(new Note(canvas, ctx, 0, 312));
-    notes.push(new Note(canvas, ctx, 3, 412));
-    notes.push(new Note(canvas, ctx, 0, 512));
-    notes.push(new Note(canvas, ctx, 3, 612));
-    notes.push(new Note(canvas, ctx, 0, 712));
-    notes.push(new Note(canvas, ctx, 3, 812));
+    notes.push(new Note(canvas, ctx, 0, -3000, 1));
+    notes.push(new Note(canvas, ctx, 0, -3000, 2));
+    notes.push(new Note(canvas, ctx, 0, -3000, 3));
+    notes.push(new Note(canvas, ctx, 0, -3000, 4));
+    notes.push(new Note(canvas, ctx, 0, -3000, 8));
 
     //存在するすべてのNoteオブジェクトの時を進める
     for (let i = 0; i < notes.length; i++) {
@@ -85,7 +85,22 @@ function frame() {
 function keypressed(e) {
     //console.log(e.key);
     if (e.code == 'KeyJ') {
-        console.log("maybe j is plassed,thanks watching this console log,");
+        //console.log("maybe j is plassed,thanks watching this console log,");
+        console.log(notes[2].falltime + (clock.getTime() - starttime));
+        judgeTiming(2);
     }
+    return false;
+}
+
+function judgeTiming(l) {
+    //bは短縮のためのインスタンスな変数です。
+    let b = (notes[2].falltime + (clock.getTime() - starttime))
+
+    console.log(`${l}is plass, i think it is${b}`);
+
+    if (300 > b && -300 < b) {
+        console.log("parfect");
+    }
+
     return false;
 }
