1ec24dec34587627a7daeb337cee4435e941dcea
asarinoudonko <kaipon110@gmail.com>
2022-04-03 22:45:18 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -21,18 +21,26 @@ var comboview;
 
 class Note {
 
-    constructor(canvas, ctx, no, falltime, hispeed, NOTE_WIDTH, START_TIME) {
+    /** @param {Number} no 0 at left */
+    constructor(ctx, no, falltime, hispeed, NOTE_WIDTH) {
 
-        this.canvas = canvas;
+        //this.canvas = canvas;
         this.ctx = ctx;
         this.no = no;
         this.hispeed = hispeed;
         this.NOTE_WIDTH = NOTE_WIDTH;
-        this.START_TIME = START_TIME;
         this.falltime = falltime;
         //(落ちるまでの時間 + 現在の時間 - 開始時間) / ハイスピ + 判定位置
         //このタイミングで現在の時間と開始時間が等しいので0
         this.y = ((falltime + 0) / hispeed) + 500;
+    }
+
+    begin(starttime) {
+        this.START_TIME = starttime;
+    }
+
+    getSTART_TIME() {
+        return this.START_TIME;
     }
 
     writenote(clock) {
@@ -45,6 +53,7 @@ class Note {
         this.ctx.fillRect(this.no * this.NOTE_WIDTH, this.y, this.NOTE_WIDTH, 10);
         //console.log(this.y);
     }
+
 
     isOVER(clock) {
         //無視された時の処理(OVER判定)
@@ -182,6 +191,30 @@ class GrooveGauge {
 
 }
 
+class MusicPlayer {
+
+    constructor() {
+        //れとろなぶらうざ用
+        const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+        this.audioContext = new AudioContext();
+
+        this.audioElement = document.querySelector('audio');
+        this.music = this.audioContext.createMediaElementSource(this.audioElement);
+
+        this.music.connect(this.audioContext.destination);
+    }
+
+    play() {
+        if (this.audioContext.state === 'suspended') {
+            this.audioContext.resume();
+        } else {
+            this.audioElement.play();
+        }
+    }
+
+}
+
 //自分自身を一度呼び出す関数っす。
 (function () {
 
@@ -189,41 +222,74 @@ class GrooveGauge {
 
 //HTML側Bodyのonlordに書かれているので、この関数はBodyの読み込みが終わったら呼ばれるはず
 function startClock() {
-
-    let notes = [];
-    let clock = new Date();
-
-    const START_TIME = clock.getTime();
-    const NOTE_WIDTH = 80;
-
-    for (i = 1; i < 70; i++) {
-        notes.push(new Note(canvas, ctx, (i + 1) % 4, -3000 - (1000 * i), i, NOTE_WIDTH, START_TIME));
-        notes.push(new Note(canvas, ctx, (i + 2) % 4, -3000 - (1000 * i), i, NOTE_WIDTH, START_TIME));
-        notes.push(new Note(canvas, ctx, (i + 3) % 4, -3000 - (1000 * i), i, NOTE_WIDTH, START_TIME));
-    }
-
-
-    for (let i = 0; i < 4; i++) {
-        bomb.push(new Bomb(ctx, i, 0, NOTE_WIDTH));
-    }
-
-    const game = new Game(notes, clock, START_TIME);
+    const game = new Game();
 }
 
 class Game {
 
-    constructor(notes, clock, START_TIME) {
-        this.notes = notes;
-        this.clock = clock;
-        this.START_TIME = START_TIME;
+    //Game開始のための準備、いろいろ読み込んでstartGameを可能にする。
+    constructor() {
+
+        this.keypresscount = 0;
 
         judgeview = new JudgeView(ctx);
         comboview = new ComboView(ctx);
 
+        this.notes = [];
+
+        //譜面をもとに、ノーツを配置する
+
+        const NOTE_WIDTH = 80;
+
+        for (let i = 1; i < 70; i++) {
+            this.notes.push(new Note(ctx, (i + 1) % 4, -3000 - (1000 * i), i, NOTE_WIDTH));
+            this.notes.push(new Note(ctx, (i + 2) % 4, -3000 - (1000 * i), i, NOTE_WIDTH));
+            this.notes.push(new Note(ctx, (i + 3) % 4, -3000 - (1000 * i), i, NOTE_WIDTH));
+        }
+
+        for (let i = 0; i < 4; i++) {
+            bomb.push(new Bomb(ctx, i, 0, NOTE_WIDTH));
+        }
+
+        this.startGame = (e) => { this._startGame(e) };
+        document.addEventListener('keydown', this.startGame);
+
+        //ゲームが実際に起動されるまで表示される待ち受け画面。
+        this.inputWaitingscreen();
+    }
+
+    //実際にゲームが始まるタイミングで呼ばれる
+    _startGame(e) {
+        this.clock = new Date();
+
+        //この関数へのアクセスを消す
+        document.removeEventListener('keydown', this.startGame);
+
+        for (let i = 0; i < this.notes.length; i++) {
+            this.notes[i].begin(this.clock.getTime());
+        }
+
         this.keypressed = (e) => { this._keypressed(e) };
         document.addEventListener('keydown', this.keypressed);
 
+        let musicplayer = new MusicPlayer();
+        musicplayer.play();
+
+        //let START_TIME = this.clock.getTime();
+
         this.IntervID = window.setInterval(this.frame.bind(this), 4);
+
+    }
+
+    //gameが実際に始まる前までに表示し続ける表示を用意します
+    inputWaitingscreen() {
+
+        this.writeBackGround();
+
+        ctx.fillStyle = 'rgb( 255, 102, 102)';
+        ctx.font = "18px serif";
+        ctx.fillText("キーボード押すと音が鳴るよ", 50, 100);
+        ctx.fillText("爆音なので注意", 50, 120);
     }
 
     //ここに、一フレームにつき行う動作を描く
@@ -232,11 +298,7 @@ class Game {
         //画面のリフレッシュ
         ctx.clearRect(0, 0, 3000, 3000);
         //背景生成
-        ctx.fillStyle = 'rgb( 0, 0, 0)';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        //判定位置生成
-        ctx.fillStyle = 'rgb( 0, 255, 0)';
-        ctx.fillRect(0, 502, canvas.width, 5);
+        this.writeBackGround();
 
         //ボム生成
         for (let i = 0; i < bomb.length; i++) {
@@ -260,8 +322,17 @@ class Game {
         //console.log((clock.getTime() - starttime) / 100);
     }
 
+    writeBackGround() {
+        ctx.fillStyle = 'rgb( 0, 0, 0)';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        //判定位置生成
+        ctx.fillStyle = 'rgb( 0, 255, 0)';
+        ctx.fillRect(0, 502, canvas.width, 5);
+    }
+
     //何らかのキーが押されている時呼ばれます
     _keypressed(e) {
+
         console.log(e.key);
         //console.log(e.key);
         if (e.repeat == false) {
@@ -279,24 +350,24 @@ class Game {
                 this.judgeTiming(3);
             }
         }
+        this.keypresscount += 1;
         return false;
     }
+
     /** @param {Number} l */
     judgeTiming(l) {
 
         //クッソ雑に全ノーツを判定します。todo。
 
         for (let i = 0; i < this.notes.length; i++) {
-            console.log(i);
             if (this.notes[i].no == l) {
 
                 //bは短縮のためのインスタンスな変数です。
 
-                let b = (this.notes[i].falltime + (this.clock.getTime() - this.START_TIME))
-
-                console.log(`${l}is plass, i think it is${b}`);
+                let b = (this.notes[i].falltime + (this.clock.getTime() - this.notes[i].getSTART_TIME()))
 
                 if (50 > b && -50 < b) {
+                    console.log(`${l}is GREAT!, i think it is${b}`);
                     console.log("GREAT");
                     judgeview.judge = "GREAT";
                     comboview.addConboCount();
