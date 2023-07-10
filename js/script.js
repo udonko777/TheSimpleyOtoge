694bd3eac563bfd4b781c6af59ebba016f4f19a5
asarinoudonko <kaipon110@gmail.com>
2023-07-10 20:48:23 +0900

    gaugeをフォルダにまとめた

--- js/script.js
+++ js/script.js
@@ -6,77 +6,10 @@ import { JudgeView } from "./components/JudgeView.mjs";
 
 import { Bomb } from "./UI/Bomb.mjs";
 
-import { Gauge } from "./Gauge.mjs";
 import { MusicPlayer } from "./MusicPlayer";
 
+import { GrooveGauge } from "./Gauges/GrooveGauge";
 
-/** TODO: ゲージのUIの実装とゲージの計算を同じ場所で行っている激ヤバClass、早く何とかする。
- * 
- */
-class GrooveGauge extends Gauge {
-
-    constructor(ctx) {
-        super(ctx);
-
-        this.groove = 22220;
-        this.GAUGE_BOX_NUMBER = 24;
-
-        this.GAUGE_BOX_AS_GROOVE = this.MAXGROOVE / this.GAUGE_BOX_NUMBER;
-
-        this.GREAT = 1000;
-        this.GOOD = 100;
-        this.BAD = -1200;
-        this.OVER = -2000;
-    }
-
-    boxcolor(no) {
-        const enableBoxNumber = Math.floor(this.groove / this.GAUGE_BOX_AS_GROOVE);
-
-        if (no < enableBoxNumber) {
-            return '#3ad132';
-        } else {
-            return '#444444';
-        }
-    }
-
-    /** ジャッジの名前からゲージの増減を計算する。ジャッジをオブジェクトにすればこんなことしなくていいと思う。
-     * @param {string} judgeName
-     */
-    set judge(judgeName) {
-        switch (judgeName) {
-            case "PGREAT":
-                this.groove += this.PGREAT;
-                break;
-            case "GREAT":
-                this.groove += this.GREAT;
-                break;
-            case "GOOD":
-                this.groove += this.GOOD;
-                break;
-            case "BAD":
-                this.groove += this.BAD;
-                break;
-            case "POOR":
-                this.groove += this.POOR;
-                break;
-            case "OVER":
-                this.groove += this.OVER;
-                break;
-            case "BREAK":
-                this.groove += this.BREAK;
-                break;
-
-            default:
-                console.log("i dont know this judgeName");
-                break;
-        }
-
-        //this.grooveを0 ~ MAXGROOVEに成型する
-        this.groove = Math.max(this.groove, 0);
-
-        this.groove = Math.min(this.groove, this.MAXGROOVE);
-    }
-}
 
 //HTML側Bodyのonlordに書かれているので、この関数はBodyの読み込みが終わったら呼ばれるはず
 globalThis.startClock = () => {
