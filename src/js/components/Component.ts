2b33f5849291aad63893ef11c40fd65825a6d00a
udowo <kaipon110@gmail.com>
2023-12-20 22:06:07 +0900

    GraphicComponentが絵を返せるように

--- src/js/components/Component.ts
+++ src/js/components/Component.ts
@@ -1,8 +1,10 @@
+import { renderableObject } from "TomoyoRender";
+
 export interface GraphicComponent {
 
     /** 
      * 理想的には毎フレーム呼ばれる描画メソッド
      */
-    draw(time: DOMHighResTimeStamp): void;
+    draw(time: DOMHighResTimeStamp): void | Array<renderableObject> | renderableObject;
 
 }
