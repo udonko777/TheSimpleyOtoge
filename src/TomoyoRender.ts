f0d6f8e54953c1288c3775395997a66d0de5b329
udowo <kaipon110@gmail.com>
2023-12-20 22:04:11 +0900

    グラフィックのインターフォース定義

--- src/TomoyoRender.ts
+++ src/TomoyoRender.ts
@@ -4,46 +4,89 @@ type HEX = `#${string}`;
 
 export type Color = RGB | RGBA | HEX;
 
-/*
- * 描画処理を集約するための一時的な実装。
- * 段階的に依存関係を解消する
- */
-export class TomoyoRender {
+//const ctx: CanvasRenderingContext2D;
 
-    private ctx: CanvasRenderingContext2D;
+//const ctx = canvas.getContext('2d');
 
+//this.ctx = ctx;
+
+//canvasの形は不定形
+//this.canvas_width = canvas.width;
+//this.canvas_height = canvas.height;
+
+
+export type Box = {
+    readonly type: `Box`,
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    style: Color,
+}
+
+export const makeBox = (x: number, y: number, width: number, height: number, style: Color): Box => {
+    return {
+        type: `Box`,
+        x,
+        y,
+        width,
+        height,
+        style
+    }
+}
+
+export type Text = {
+    readonly type: `Text`,
+    text: string,
+    x: number,
+    y: number,
+    font: string,
+    style: Color,
+}
+
+export const makeText = (text: string, x: number, y: number, font: string, style: Color): Text => {
+    return {
+        type: `Text`,
+        text,
+        x,
+        y,
+        font,
+        style
+    }
+}
+
+export type renderableObject = Box | Text
+
+export type ScreenModel = Readonly<{
+    ctx: CanvasRenderingContext2D;
     canvas_width: number;
     canvas_height: number;
+}>
 
-    constructor(canvas:HTMLCanvasElement) {
-
-        const ctx = canvas.getContext('2d');
-
-        if(!ctx){
-            throw new Error();
-        }
-
-        this.ctx = ctx;
-
-        //canvasの形は不定形
-        this.canvas_width = canvas.width;
-        this.canvas_height = canvas.height;
+export const draw = (Screen: ScreenModel, graphic: renderableObject) => {
+    switch (graphic.type) {
+        case "Box":
+            drawBox(Screen, graphic);
+            break
+        case "Text":
+            drawText(Screen, graphic);
+            break
     }
+}
 
-    /** `ctx.fillRect`の代わりに用意された描画メソッド */
-    public drawBox(x: number, y: number, width: number, height: number, style: Color) {
-        this.ctx.fillStyle = style;
-        this.ctx.fillRect(x, y, width, height);
-    }
+/** `ctx.fillRect`の代わりに用意された描画メソッド */
+const drawBox = (Screen: ScreenModel, box: Box) => {
+    Screen.ctx.fillStyle = box.style;
+    Screen.ctx.fillRect(box.x, box.y, box.width, box.height);
+}
 
-    /** `ctx.fillText`の代わりに用意された描画メソッド */
-    public drawText(text: string, x: number, y: number, font: string, style: Color) {
-        this.ctx.fillStyle = style;
-        this.ctx.font = font;
-        this.ctx.fillText(text, x, y);
-    }
+/** `ctx.fillText`の代わりに用意された描画メソッド */
+const drawText = (Screen: ScreenModel, text: Text) => {
+    Screen.ctx.fillStyle = text.style;
+    Screen.ctx.font = text.font;
+    Screen.ctx.fillText(text.text, text.x, text.y);
+}
 
-    public clear() {
-        this.ctx.clearRect(0, 0, this.canvas_width, this.canvas_height);
-    }
+export const clear = (Screen: ScreenModel) => {
+    Screen.ctx.clearRect(0, 0, Screen.canvas_width, Screen.canvas_height);
 }
